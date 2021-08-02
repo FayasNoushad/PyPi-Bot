a4b784e3d08cc2e54b19e3b682dde1ac4de98648
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-08-02 18:48:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def pypi_text(query):
     text += f"\n**Package Name:** `{info['PackageName']}`"
     text += f"\n**Title:** `{info['Title']}`"
     text += f"\n**About:** `{info['About']}`"
-    text += f"\n**Latest Release Date:**`{info['LatestReleaseDate']}`"
+    text += f"\n**Latest Release Date:** `{info['LatestReleaseDate']}`"
     text += f"\n**PiP Command:** `{info['PipCommand']}`"
     return text
 
